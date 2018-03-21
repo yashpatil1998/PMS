057636e0ca247a971d403a5bc467a9309a3df7f8
Yash Patil <35997847+yashpatil1998@users.noreply.github.com>
2018-03-21 20:28:47 +0530

    Added editUserDetails()
    Can now change user name

--- src/com/yhp/pms/User.java
+++ src/com/yhp/pms/User.java
@@ -18,5 +18,21 @@ public class User {
 	public void deletePortfolio(String pName) {
 		mapPortfolio.remove(pName);
 	}
+	
+	public void addPortfolio() {
+
+	}
+	
+	public void editUserDetails(Scanner scanner) {
+		System.out.println("Enter \n1 to edit UserName\n");
+		int choice = scanner.nextInt();
+		switch (choice) {
+		case 1:
+			System.out.println("Enter new UserName:");
+			String newUserName = scanner.next();
+			this.userName = newUserName;
+			break;
+		}
+	}
 
 }
