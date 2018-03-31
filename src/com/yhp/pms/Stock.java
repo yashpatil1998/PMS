3c2f0018c44b271315a26bc320fb021fb9ad7016
Yash Patil <35997847+yashpatil1998@users.noreply.github.com>
2018-03-31 11:53:43 +0530

    Can get RealTime Price from AlphaVantage API

--- src/com/yhp/pms/Stock.java
+++ src/com/yhp/pms/Stock.java
@@ -3,17 +3,18 @@ package com.yhp.pms;
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.*;
 
 public class Stock {
 
 	private int stockId;
-	private float currentPrice;
+	private float currentPriceOpen;
+	private float currentPriceClose;
 	private String stockName;
 
-	public Stock(int stockId, int currentPrice, String stockName) {
+	public Stock(int stockId, int currentPriceOpen, String stockName) {
 		this.stockId = stockId;
-		this.currentPrice = currentPrice;
+		this.currentPriceOpen = currentPriceOpen;
 		this.stockName = stockName;
 	}
 
@@ -25,35 +26,59 @@ public class Stock {
 		this.stockId = stockId;
 	}
 
-	public float getCurrentPrice() throws IOException {
-		String stockPrice = "0.0000";
+	public float getCurrentPriceOpen(Scanner sc) throws IOException {
+		String stockPriceOpen = "0.0000";
+		String stockPriceClose = "0.0000";
 //		String timeStamp = new SimpleDateFormat("yyyy-MM-dd")
 //				.format(Calendar.getInstance().getTime());
-//		System.out.println(timeStamp);
-		String stockSymbol = "MSFT";
+		System.out.println("Enter year yyyy");
+		int y = sc.nextInt();
+		System.out.println("Enter month mm");
+		int m = sc.nextInt();
+		System.out.println("Enter day dd");
+		int d = sc.nextInt();
+		System.out.println("Enter Stock Symbol");
+		String stockSymbol = sc.next();
 		String link = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
 				+ stockSymbol + "&outputsize=full&apikey=17HLSDZ8O8VT0GL0.json";
 		URL url = new URL(link);
 		URLConnection urlConn = url.openConnection();
 		BufferedReader br = new BufferedReader(new InputStreamReader(
 				urlConn.getInputStream()));
-		String line = br.readLine();
-		int start = 0;
-		while (line != null) {
-			if (line.contains("1. open")) {
-				start = line.indexOf(".", line.indexOf(".") + 1);
-				stockPrice = line.substring(start - 2, start + 5);
-				break;
+		String line1 = br.readLine();
+		int startOpen = 0;
+		int choice = 1;
+		switch (choice) {
+		case 1:
+			while (line1 != null) {
+				while (line1.contains(y + "-" + m + "-" + d) == true) {
+
+					while (line1.contains("1. open") == false) {
+						line1 = br.readLine();
+
+						if (line1.contains("1. open") == true) {
+							startOpen = line1.indexOf(".",
+									line1.indexOf(".") + 1);
+							stockPriceOpen = line1.substring(startOpen - 2,
+									startOpen + 5);
+							System.out.println("Check Open");
+							break;
+						}
+
+					}
+				}
+				line1 = br.readLine();
 			}
-			
-			line = br.readLine();
+			break;
 		}
-		this.currentPrice = Float.parseFloat(stockPrice);
-		return currentPrice;
+		System.out.println(stockPriceOpen);
+		System.out.println(stockPriceClose);
+		this.currentPriceOpen = Float.parseFloat(stockPriceOpen);
+		return currentPriceOpen;
 	}
 
 	public void setCurrentPrice(int currentPrice) {
-		this.currentPrice = currentPrice;
+		this.currentPriceOpen = currentPrice;
 	}
 
 	public String getStockName() {
@@ -66,7 +91,7 @@ public class Stock {
 
 	@Override
 	public String toString() {
-		return "Stock [stockId=" + stockId + ", currentPrice=" + currentPrice
+		return "Stock [stockId=" + stockId + ", currentPrice=" + currentPriceOpen
 				+ ", stockName=" + stockName + "]";
 	}
 
