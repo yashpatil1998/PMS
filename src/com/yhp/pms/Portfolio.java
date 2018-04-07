e3b3a37bb332d45f5ccf1fa1e00e018890114dac
Yash Patil <35997847+yashpatil1998@users.noreply.github.com>
2018-04-07 21:40:45 +0530

    Update Portfolio.java

--- src/com/yhp/pms/Portfolio.java
+++ src/com/yhp/pms/Portfolio.java
@@ -1,84 +1,75 @@
 package com.yhp.pms;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
-public class User {
-	private Map<String, Portfolio> mapPortfolio = new LinkedHashMap<String, Portfolio>();
-	private String userName;
-	private int userId;
-	private long netWorth;
 
-	@Override
-	public String toString() {
-		return "User [userName=" + userName + ", userId=" + userId
-				+ ", netWorth=" + netWorth + "]";
-	}
+public class Portfolio {
 
-	public Map<String, Portfolio> getMapPortfolio() {
-		return mapPortfolio;
-	}
-
-	public void setMapPortfolio(Map<String, Portfolio> mapPortfolio) {
-		this.mapPortfolio = mapPortfolio;
-	}
-
-	public User(String userName, int userId, long netWorth) {
-		this.userName = userName;
-		this.userId = userId;
-		this.netWorth = netWorth;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	public long getNetWorth() {
+	private String portfolioName;
+	private Map<String, Stock> mapStock = new LinkedHashMap<String, Stock>();
+	private float netWorth = 0.0f;
+	
+	
+	
+	public float getNetWorth() {
 		return netWorth;
 	}
 
-	public void setNetWorth(long netWorth) {
-		this.netWorth = netWorth;
+	public void setNetWorth(float f) {
+		this.netWorth = f;
 	}
 
-	public void renamePortfolio(String oldName, String newName) {
-
-		Portfolio portfolio = mapPortfolio.get(oldName);
-		portfolio.setPortfolioName(newName);
-
-	}
-
-	public void deletePortfolio(String pName) {
-		mapPortfolio.remove(pName);
-	}
-
-	public void addPortfolio(String name) {
-
-		mapPortfolio.put(name, new Portfolio(name));
-
-	}
-
-	public void editUserDetails(Scanner scanner) {
-		System.out.println("Enter \n1 to edit UserName\n");
-		int choice = scanner.nextInt();
-		switch (choice) {
-		case 1:
-			System.out.println("Enter new UserName:");
-			String newUserName = scanner.next();
-			this.userName = newUserName;
-			break;
+	public void getStockSymbol() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("StockSymbol.txt"));
+		String read = br.readLine();
+		while (read != null) {
+			System.out.println(read);
+			read = br.readLine();
 		}
+		br.close();
+		
 	}
+	
+	public void getExistingSecurities() {
+		System.out.println(this.mapStock.keySet());
+	}
+
+	public String getPortfolioName() {
+		return portfolioName;
+	}
+
+	public void setPortfolioName(String portfolioName) {
+		this.portfolioName = portfolioName;
+	}
+
+	public Portfolio(String portfolioName) {
+		this.portfolioName = portfolioName;
+	}
+
+	public void addNewSecurity(String stockSymbol, Scanner sc) {
+
+		Stock activeStock = new Stock(stockSymbol);
+		mapStock.put(stockSymbol, activeStock);
+
+	}
+
+	public void deleteSecurity(String sym) {
+		mapStock.remove(sym);
+	}
+
+	public Map<String, Stock> getMapStock() {
+		return mapStock;
+	}
+
+	@Override
+	public String toString() {
+		return "Portfolio [portfolioName=" + portfolioName + ", mapStock="
+				+ mapStock + "]";
+	}
+	
+	
 
 }
