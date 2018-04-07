f64645b267c7cdf211572f6dc20df1a1396fcbe3
Yash Patil <35997847+yashpatil1998@users.noreply.github.com>
2018-04-07 15:05:17 +0530

    Update Portfolio.java

--- src/com/yhp/pms/Portfolio.java
+++ src/com/yhp/pms/Portfolio.java
@@ -1,78 +1,84 @@
 package com.yhp.pms;
 
-import java.io.IOException;
 import java.util.*;
 
-public class Portfolio {
-
-	private List<Security> securityList = new ArrayList<Security>();
-	private String portfolioName;
-	
-	
-
-	public List<Security> getSecurityList() {
-		return securityList;
-	}
-
-	public String getPortfolioName() {
-		return portfolioName;
-	}
-
-	public void setPortfolioName(String portfolioName) {
-		this.portfolioName = portfolioName;
-	}
-
-	public Portfolio(String portfolioName) {
-		this.portfolioName = portfolioName;
-	}
-
+public class User {
+	private Map<String, Portfolio> mapPortfolio = new LinkedHashMap<String, Portfolio>();
+	private String userName;
+	private int userId;
+	private long netWorth;
 
 	@Override
 	public String toString() {
-		return "Portfolio [securityList=" + securityList + ", portfolioName="
-				+ portfolioName + "]";
+		return "User [userName=" + userName + ", userId=" + userId
+				+ ", netWorth=" + netWorth + "]";
 	}
 
-	public void addSecurity(String type, Scanner sc) throws IOException {
-		
-		securityList.add(new Security(type));
-		Security security = securityList.get(securityList.size() - 1);
-		if (type.equalsIgnoreCase("Stock")) {
-			System.out.println("Enter stock name (Symbol) :");
-			String sName = sc.next();
-			System.out.println("Enter quantity :");
-			int sQuant = sc.nextInt();
-			Stock stock = new Stock(security.getStockList().size(), 0, sName);
-			float sprice = stock.getCurrentPriceOpen(sc);
-			float sAmount = sprice * sQuant;
-			System.out.println("Total stock price is :"+sAmount);
-			security.addTransaction(security.getTransactionList().size(),
-					sAmount, sQuant, type, sName, "");
-		} else {
-			System.out.println("Enter Bank name :");
-			String bName = sc.next();
-			System.out.println("Enter Account Name :");
-			String aName = sc.next();
-			System.out.println("Enter account balance :");
-			int aBalance = sc.nextInt();
-			security.addTransaction(security.getTransactionList().size(),
-					aBalance, 0, type, aName, bName);
+	public Map<String, Portfolio> getMapPortfolio() {
+		return mapPortfolio;
+	}
+
+	public void setMapPortfolio(Map<String, Portfolio> mapPortfolio) {
+		this.mapPortfolio = mapPortfolio;
+	}
+
+	public User(String userName, int userId, long netWorth) {
+		this.userName = userName;
+		this.userId = userId;
+		this.netWorth = netWorth;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public long getNetWorth() {
+		return netWorth;
+	}
+
+	public void setNetWorth(long netWorth) {
+		this.netWorth = netWorth;
+	}
+
+	public void renamePortfolio(String oldName, String newName) {
+
+		Portfolio portfolio = mapPortfolio.get(oldName);
+		portfolio.setPortfolioName(newName);
+
+	}
+
+	public void deletePortfolio(String pName) {
+		mapPortfolio.remove(pName);
+	}
+
+	public void addPortfolio(String name) {
+
+		mapPortfolio.put(name, new Portfolio(name));
+
+	}
+
+	public void editUserDetails(Scanner scanner) {
+		System.out.println("Enter \n1 to edit UserName\n");
+		int choice = scanner.nextInt();
+		switch (choice) {
+		case 1:
+			System.out.println("Enter new UserName:");
+			String newUserName = scanner.next();
+			this.userName = newUserName;
+			break;
 		}
-
-	}
-
-	public void deleteSecurity(Scanner sc) {
-			System.out.println("Enter security ID :");
-			int sId = sc.nextInt();
-			this.securityList.remove(sId);
-	}
-	
-	public void viewSecurity(int sId) {
-		System.out.println(this.securityList.get(sId));
-	}
-
-	public int calculateNetWorth() {
-		return 3;
 	}
 
 }
